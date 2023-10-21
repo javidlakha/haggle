4bb5bfe06436b6db0592310f8059cab554b1a03b
Henry Maguire <henrymaguire@hotmail.co.uk>
2023-10-21 17:07:00 +0100

    Fix broken settings

--- api/settings.py
+++ api/settings.py
@@ -1,17 +1,13 @@
 import pathlib
 from starlette.config import Config
 
-<<<<<<< HEAD
 BASE_PATH = pathlib.Path(__file__).parent.parent.resolve()
-=======
-# Configuration from environment variables or '.env' file.
-config = Config(pathlib.Path(__file__).parent.parent.resolve() / ".env")
-
-# GCP API key.
-GCP_API_KEY = config("GCP_API_KEY", default="")
->>>>>>> master
 
 # Configuration from environment variables or '.env' file.
 config = Config(BASE_PATH / ".env")
+
 # OpenAI API key.
 OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
+
+# GCP API key.
+GCP_API_KEY = config("GCP_API_KEY", default="")
