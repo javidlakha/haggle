aeed2037d2a38c20dad4b331e7aa700a73ada844
Henry Maguire <henrymaguire@hotmail.co.uk>
2023-10-21 17:04:11 +0100

    Add openai call

--- api/index.py
+++ api/index.py
@@ -1,14 +1,56 @@
+from enum import Enum, unique
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
+from langchain.chat_models import ChatOpenAI
+from langchain.chat_models.openai import acompletion_with_retry
+
 from api.settings import OPENAI_API_KEY
 
 app = FastAPI()
 
 
+@unique
+class ModelType(str, Enum):
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
+    GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k"
+    GPT_3_5_TURBO_0613 = "gpt-3.5-turbo-0613"
+    GPT_4 = "gpt-4"
+    GPT_4_0613 = "gpt-4-0613"
+    GPT_4_32K_0613 = "gpt-4-32k-0613"
+
+
 class SubmitMessageRequest(BaseModel):
     message: str
+    messages: list[str] = []
+
 
 
 @app.post("/api/chat.submit")
-def submit(body: SubmitMessageRequest):
-    return {"message": body.message[::-1]}
+async def submit(body: SubmitMessageRequest):
+    llm = ChatOpenAI(
+        client=None,
+        model=ModelType.GPT_3_5_TURBO,
+        temperature=0.0,
+        streaming=False,
+        max_retries=2,
+        request_timeout=240,
+        openai_api_key=OPENAI_API_KEY,
+    )
+    messages = [
+        {
+            "role": "system",
+            "content": "You are an interviewer. Stay in character!"
+        },
+        {
+            "role": "user",
+            "content": body.message,
+        },
+    ]
+    output = await acompletion_with_retry(
+        llm=llm,
+        model=ModelType.GPT_3_5_TURBO,
+        messages=messages,
+    )
+    
+    return output["choices"][0]["message"]
--- api/settings.py
+++ api/settings.py
@@ -1,8 +1,9 @@
 import pathlib
 from starlette.config import Config
 
-# Configuration from environment variables or '.env' file.
-config = Config(pathlib.Path.cwd() / ".env")
+BASE_PATH = pathlib.Path(__file__).parent.parent.resolve()
 
+# Configuration from environment variables or '.env' file.
+config = Config(BASE_PATH / ".env")
 # OpenAI API key.
 OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
