192daee69fa15560e91b504fe1c965025acb49cc
Henry Maguire <henrymaguire@hotmail.co.uk>
2023-10-21 22:56:46 +0100

    Working wired up app

--- api/index.py
+++ api/index.py
@@ -5,10 +5,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 from langchain.chat_models.openai import acompletion_with_retry
-
-from api.settings import OPENAI_API_KEY
+from fastapi import FastAPI, File, UploadFile
+from pathlib import Path
+from api.settings import BASE_PATH, OPENAI_API_KEY
 from api.voice import Accent, text_to_speech
-
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.document_loaders.image import UnstructuredImageLoader
 
 app = FastAPI()
 
@@ -23,26 +25,17 @@ class ModelType(str, Enum):
     GPT_4_32K_0613 = "gpt-4-32k-0613"
 
 
-class SubmitMessageRequest(BaseModel):
-    message: str
-    messages: list[str] = []
+# TODO(hm): Overwrite all of these based on user input.
 
+COMPANY = "Google"
 
-characters = [
-    {
-        "name": "Brian",
-        "role": "Head of Engineering",
-        "accent": "British",
-        "personality": "aggressive, impatient, pedant",
-    },
-]
-
-cv = """
-I'm Henry a software engineer with 5 years of experience.
-I have worked at Google and Facebook. I have a degree in Computer Science from MIT.
+DEFAULT_CV = """
+I'm Ally a software engineer with 5 years of experience.
+I have worked at Delloite and Barclays. I used to be a professional poker player 
+and I before that I got a degree in philosophy from the University of Bath.
 """
 
-job_description = """
+DEFAULT_JOB_DESCRIPTION = """
 We are looking for a software engineer with 5 years of experience.
 You will be working on our new product.
 """
@@ -54,27 +47,133 @@ Here is the candidate's CV: {cv}
 
 Here is the job description: {job_description}
 
-Below, you may see a conversation history between you and the candidate.
+This is some extra context about the candidate: {user_context}
+
+Below, you may see a conversation history between you, the candidate and these other characters: {other_characters}
 
 Conduct an interview with the candidate. Always stay in character.
 """
 
-random_character = random.choice(characters)
 
-system_message = SYSTEM_MESSAGE.format(
-    character_name=random_character["name"],
-    character_role=random_character["role"],
-    character_personality=random_character["personality"],
-    company_name="Google",
-    cv = cv,
-    job_description = job_description,
-)
-    
-messages = [{"role": "system", "content": system_message}]
+class SubmitMessageRequest(BaseModel):
+    message: str
+    messages: list[str] = []
+
+
+class InitChatRequest(BaseModel):
+    user_context: str = ""
+
+
+# Fake database.
+cv = DEFAULT_CV
+job_description = DEFAULT_JOB_DESCRIPTION
+user_context = None
+messages = []
+
+# TODO(hm): Autogenerate these from the JD.
+characters = [
+    {
+        "name": "Janet",
+        "role": "Head of Engineering",
+        "accent": "British",
+        "personality": "aggressive, impatient and a pedant",
+        "color": "red",
+        "img": "bot",
+    },
+    {
+        "name": "Brian",
+        "role": "Product Manager",
+        "accent": "Irish",
+        "personality": "friendly, helpful and a good listener",
+        "color": "orange",
+        "img": "bot",
+    },
+]
+
+
+def load_documents(file_path, is_pdf):
+    if is_pdf:
+        loader = PyMuPDFLoader(file_path)
+    else:
+        loader = UnstructuredImageLoader(file_path)
+
+    documents = loader.load()
+    return documents
+
+
+def get_document_string(file_path, is_pdf):
+    print("file path is: ", file_path)
+    documents = load_documents(file_path, is_pdf)
+    document_str = ""
+    for doc in documents:
+        content = doc.page_content
+        document_str += "\n" + content + "\n"
+    return document_str
+
+
+@app.post("/api/chat.upload-doc")
+async def upload_file(file: UploadFile):
+    # Define the path to save the file
+    file_path = BASE_PATH / f"buckets/document-uploads/{file.filename}"
+
+    with file_path.open("wb") as buffer:
+        # Use the 'for' syntax to iterate through the file's contents
+        buffer.write(await file.read())
+
+    output = get_document_string(str(file_path), is_pdf=file.filename.endswith(".pdf"))
+
+    with file_path.with_suffix(".txt").open("w") as fp:
+        fp.write(output)
+    if "cv" in file.filename:
+        global cv
+        cv = output
+    else:
+        global job_description
+        job_description = output
+
+    return {"filename": file.filename}
+
+
+@app.post("/api/chat.init")
+async def init(body: InitChatRequest):
+    # TODO(hm): Get the persons name and company name from the CV/JD.
+    global messages
+    global characters
+    global user_context
+
+    user_context = body.user_context
+
+    init_message = f"Hi, Confident-Ally, welcome to {COMPANY}. Shall we begin by doing some introductions?"
+
+    if cv is None:
+        init_message += " We see you didn't provide a CV."
+
+    initial_message = {
+        "role": "assistant",
+        "content": init_message,
+        "character": random.choice(characters),
+    }
+    messages.append(initial_message)
+    return {
+        "initial_message": initial_message,
+        "characters": characters,
+    }
 
 
 @app.post("/api/chat.submit")
 async def submit(body: SubmitMessageRequest):
+    random_character = random.choice(characters)
+    other_characters = ", ".join([f'{c["name"]}: {c["role"]}' for c in characters if c["name"] != random_character["name"]])
+    system_message = SYSTEM_MESSAGE.format(
+        character_name=random_character["name"],
+        character_role=random_character["role"],
+        character_personality=random_character["personality"],
+        company_name=COMPANY,
+        cv=cv,
+        job_description=job_description,
+        user_context=user_context,
+        other_characters=other_characters,
+    )
     llm = ChatOpenAI(
         client=None,
         model=ModelType.GPT_3_5_TURBO,
@@ -85,15 +184,21 @@ async def submit(body: SubmitMessageRequest):
         openai_api_key=OPENAI_API_KEY,
     )
     global messages
+
     messages.append({"role": "user", "content": body.message})
+
+    _messages = [{"role": "system", "content": system_message}] + [
+        {"role": m["role"], "content": m["content"]} for m in messages
+    ]
+
     resp = await acompletion_with_retry(
         llm=llm,
         model=ModelType.GPT_3_5_TURBO,
-        messages=messages,
+        messages=_messages,
     )
     output = resp["choices"][0]["message"]
     messages.append(output)
-    return output
+    return {"message": output["content"], "character": random_character}
 
 
 # TODO: May not need to expose this
--- app/danny/chat.tsx
+++ app/danny/chat.tsx
@@ -1,23 +1,65 @@
-"use client";
-import React, { useState } from "react";
-import "./chat.css";
-import Image from "next/image";
-import { outdent } from "outdent";
+"use client"
+import React, { useState } from "react"
+import "./chat.css"
+import Image from "next/image"
+import { outdent } from "outdent"
 
-type Side = "left" | "right";
+type Side = "left" | "right"
 
 type Message = {
-  name: string;
-  img: "bot" | "person";
-  side: Side;
-  date: Date;
-  text: string;
-};
+  name: string
+  img: "bot" | "person"
+  side: Side
+  date: Date
+  text: string
+}
 function formatDate(date: Date) {
-  const h = "0" + date.getHours();
-  const m = "0" + date.getMinutes();
+  const h = "0" + date.getHours()
+  const m = "0" + date.getMinutes()
 
-  return `${h.slice(-2)}:${m.slice(-2)}`;
+  return `${h.slice(-2)}:${m.slice(-2)}`
+}
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0]
+    setSelectedFile(file)
+  }
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const formData = new FormData()
+      formData.append("file", selectedFile)
+
+      try {
+        const response = await fetch("/api/chat.upload-doc", {
+          method: "POST",
+          body: formData,
+        })
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText)
+        }
+
+        const data = await response.json()
+        console.log("File uploaded:", data.filename)
+      } catch (error) {
+        console.error("There has been a problem with your fetch operation:", error)
+      }
+    }
+  }
+
+  return (
+    <div className="file-upload">
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload} disabled={!selectedFile}>
+        Upload
+      </button>
+      {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+    </div>
+  )
 }
 
 const Msg = (message: Message) => {
@@ -46,299 +88,197 @@ const Msg = (message: Message) => {
         <div className="msg-text">{message.text}</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-const BOT_MSGS = [
-  "Hi, how are you?",
-  "Ohh... I can't understand what you trying to say. Sorry!",
-  "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
-  "I feel sleepy! :(",
-];
-
-const BOT_IMG = "bot";
-const PERSON_IMG = "person";
-const BOT_NAME = "Big Boss";
-const PERSON_NAME = "Confident-Ally";
+const BOT_IMG = "bot"
+const PERSON_IMG = "person"
+const PERSON_NAME = "Confident-Ally"
+const DEFAULT_CONTEXT = "I'm applying for a job as a software engineer at Meta. I struggle with waffling."
 
 type Panellist = {
-  img: "bot" | "person";
-  name: string;
-  description: string;
-  role: "boss" | "assistant";
-  color: "red" | "orange" | "green";
-};
-
-const PANELLISTS: Panellist[] = [
-  {
-    img: "bot",
-    name: "Big Boss",
-    description:
-      "This person is responsible for the final decision. They should only talk whenever the user talks about an animal or tries to end the interview",
-    role: "assistant",
-    color: "red",
-  },
-  {
-    img: "bot",
-    name: "Medium Boss",
-    description:
-      "This person is should talk whenever the user asks a question.",
-    role: "assistant",
-    color: "orange",
-  },
-  {
-    img: "bot",
-    name: "Small Boss",
-    description:
-      "This person should ask a follow-on question whenever the user answers a question.",
-    role: "assistant",
-    color: "green",
-  },
-];
-
-async function whoShouldRespond(
-  options: Panellist[],
-  conversation: string,
-  roleplaySetup?: string
-): Promise<string> {
-  const systemPrompt = outdent`
-  From an ongoing conversation between a user and some panellists, decide which panellist should respond next.
-  Here are the descriptions of the panellists:
-  ${options.map((option) => `${option.name}: ${option.description}`).join("\n")}
-
-  Here is the conversation so far:
-  ${conversation}
-
-  Return only the name of the panellist who should respond next. Ie one of: ${options
-    .map((option) => option.name)
-    .join(", ")}
-  `;
-
-  const whoShouldRespond = await fetch(
-    "https://api.openai.com/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo-16k",
-        messages: [{ role: "system", content: systemPrompt }],
-        temperature: 0,
-      }),
-    }
-  );
-
-  const responderData = await whoShouldRespond.json();
-  return responderData.choices[0].message.content;
+  img: "bot" | "person"
+  name: string
+  description: string
+  role: "boss" | "assistant"
+  color: "red" | "orange" | "green"
 }
 
-async function getResponse(conversation: Message[], roleplaySetup?: string) {
-  const convoForResponseDecision = conversation
-    .map((msg) => ({
-      content: `${msg.name}: ${msg.text}`,
-    }))
-    .join("\n");
-  const responder = await whoShouldRespond(
-    PANELLISTS,
-    convoForResponseDecision,
-    roleplaySetup
-  );
-
-  console.log({ responder });
-
-  const messagesForOpenAI = conversation.map((msg) => ({
-    role: msg.side === "left" ? "assistant" : "user",
-    content: msg.text,
-  }));
-
-  const finishedMessages = roleplaySetup
-    ? [
-        {
-          role: "system",
-          content: `${roleplaySetup} \n Now respond as ${responder}`,
-        },
-        ...messagesForOpenAI,
-      ]
-    : messagesForOpenAI;
-  console.log({ finishedMessages });
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo-16k",
-      messages: roleplaySetup
-        ? [{ role: "system", content: roleplaySetup }, ...messagesForOpenAI]
-        : messagesForOpenAI,
-      temperature: 0,
-    }),
-  });
-  const data = await response.json();
-  const message = data.choices[0].message.content;
-  console.log({ message });
-  return {
-    text: message,
-    name: responder,
-  };
-}
 
 export const Chat = () => {
-  const [roleplaySetup, setRoleplaySetup] = useState<string>(
-    `You are an AI assistant helping the user to roleplay a job interview situation. You are playing the role of several panellists who are interviewing the user who is a job applicant. The panellists are: \n ${PANELLISTS.map(
-      (panellist) => `${panellist.name}: ${panellist.description}`
-    ).join("\n")}
-    Your messages should be conversational and fit th tone of a job interviewer. They should be short and polite.`
-  );
-  const [msgText, setMsgText] = useState("");
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      name: BOT_NAME,
-      img: BOT_IMG,
-      side: "left",
-      text: "Hi, welcome to this job interview!",
-      date: new Date(),
-    },
-  ])
+  const [roleplaySetup, setRoleplaySetup] = useState<string>(DEFAULT_CONTEXT)
+  const [msgText, setMsgText] = useState("")
+  const [isInitialised, setIsInitialised] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+  const [panellists, setPanellists] = useState<Panellist[]>([])
 
-  function appendMessage(message: Message) {
-    setMessages((prev) => [...prev, message]);
+  async function initChat(message: string) {
+    try {
+      const response = await fetch("/api/chat.init", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_context: message }),
+      })
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText)
+      }
+      const data = await response.json()
+      setPanellists(data.characters)
+      appendMessage({
+        name: data.initial_message.character.name,
+        img: BOT_IMG,
+        side: "left",
+        text: data.initial_message.content,
+        date: new Date(),
+      })
+    } catch (error) {
+      console.error("There has been a problem with your fetch operation:", error)
+    }
   }
 
-  async function remoteBotResponse(message: string) {
+  function appendMessage(message: Message) {
+    setMessages((prev) => [...prev, message])
+  }
+
+  async function remoteBotResponse(message: Message) {
     try {
       const response = await fetch("/api/chat.submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ message }),
-      });
+        body: JSON.stringify({ message: message.text }),
+      })
       if (!response.ok) {
-        throw new Error("Network response was not ok " + response.statusText);
+        throw new Error("Network response was not ok " + response.statusText)
       }
-      const data = await response.json();
-      const msgText = data.content;
-
+      const data = await response.json()
       appendMessage({
-        name: BOT_NAME,
+        name: data.character.name,
         img: BOT_IMG,
         side: "left",
-        text: msgText,
+        text: data.message,
         date: new Date(),
-      });
+      })
     } catch (error) {
-      console.error(
-        "There has been a problem with your fetch operation:",
-        error
-      );
+      console.error("There has been a problem with your fetch operation:", error)
     }
-  }
-
-  function localBotResponse(message: Message) {
-    const messagesToSubmit =
-      messages[messages.length - 1].text === message.text
-        ? messages
-        : [...messages, message];
-    getResponse(messagesToSubmit).then((res) => {
-      appendMessage({
-        name: res.name,
-        img: BOT_IMG,
-        side: "left",
-        text: res.text,
-        date: new Date(),
-      });
-    });
   }
 
   return (
     <div>
-      <input
-        className="text-black"
-        value={roleplaySetup}
-        onChange={(e) => setRoleplaySetup(e.target.value)}
-      />
-      {PANELLISTS.map((panellist) => {
+      {!isInitialised && (
+        <>
+          <section className="msger">
+            <header className="msger-header">
+              <div className="msger-header-options">
+                <span>
+                  <i className="fas fa-cog"></i>
+                </span>
+              </div>
+            </header>
+            <form
+              className="msger-inputarea"
+              onSubmit={async (e) => {
+                e.preventDefault()
+                if (!roleplaySetup) return
+
+                setIsInitialised(true)
+                initChat(roleplaySetup)
+              }}
+            >
+              <textarea
+                className="msger-input text-black"
+                style={{ margin: "auto" }}
+                value={roleplaySetup}
+                onChange={(e) => setRoleplaySetup(e.target.value)}
+              />
+              <button type="submit" className="msger-send-btn">
+                Send
+              </button>
+            </form>
+          </section>
+          <FileUpload />
+        </>
+      )}
+      {panellists.map((panellist) => {
         return (
           // Little icons and names of the panellist
           <div
-            key={panellist.name}
-            style={{ color: panellist.color }}
-            className={`text-${panellist.color} flex items-center gap-2`}
+            key={panellist?.name}
+            style={{ color: panellist?.color }}
+            className={`text-${panellist?.color} flex items-center gap-2`}
           >
             <Image
               priority
-              src={`/${panellist.img}.svg`}
-              style={{ color: panellist.color }}
+              src={`/${panellist?.img}.svg`}
+              style={{ color: panellist?.color }}
               className="mt-2 ml-2"
               width={30}
               height={30}
-              color={panellist.color}
+              color={panellist?.color}
               alt="Follow us on Twitter"
             />
-            <div className={`text-${panellist.color}`}>{panellist.name}</div>
-            <div>{panellist.description}</div>
+            <div className={`text-${panellist?.color}`}>{panellist?.name}</div>
+            <div>: {panellist?.role}</div>
           </div>
-        );
+        )
       })}
-      <section className="msger">
-        <header className="msger-header">
-          <div className="msger-header-title">HagglChat</div>
-          <div className="msger-header-options">
-            <span>
-              <i className="fas fa-cog"></i>
-            </span>
-          </div>
-        </header>
+      {isInitialised && (
+        <section className="msger">
+          <header className="msger-header">
+            <div className="msger-header-title">HagglChat</div>
+            <div className="msger-header-options">
+              <span>
+                <i className="fas fa-cog"></i>
+              </span>
+            </div>
+          </header>
 
-        <main
-          style={{ maxHeight: "500px" }}
-          className="msger-chat overflow-y-scroll "
-        >
-          {messages.map(Msg)}
-        </main>
+          <main style={{ maxHeight: "500px" }} className="msger-chat overflow-y-scroll ">
+            {messages.map(Msg)}
+          </main>
 
-        <form
-          className="msger-inputarea"
-          onSubmit={async (e) => {
-            e.preventDefault();
-            if (!msgText) return;
+          <form
+            className="msger-inputarea"
+            onSubmit={async (e) => {
+              e.preventDefault()
+              if (!msgText) return
 
-            appendMessage({
-              name: PERSON_NAME,
-              img: PERSON_IMG,
-              side: "right",
-              text: msgText,
-              date: new Date(),
-            });
-            // TODO: see if wait is required here
-            setMsgText("");
+              appendMessage({
+                name: PERSON_NAME,
+                img: PERSON_IMG,
+                side: "right",
+                text: msgText,
+                date: new Date(),
+              })
+              // TODO: see if wait is required here
+              setMsgText("")
 
-            localBotResponse({
-              name: PERSON_NAME,
-              img: PERSON_IMG,
-              side: "right",
-              text: msgText,
-              date: new Date(),
-            });
-          }}
-        >
-          <input
-            type="text"
-            className="msger-input text-black"
-            placeholder="Enter your message..."
-            value={msgText}
-            onChange={(e) => setMsgText(e.target.value)}
-          />
-          <button type="submit" className="msger-send-btn">
-            Send
-          </button>
-        </form>
-      </section>
+              remoteBotResponse({
+                name: PERSON_NAME,
+                img: PERSON_IMG,
+                side: "right",
+                text: msgText,
+                date: new Date(),
+              })
+            }}
+          >
+            <input
+              type="text"
+              className="msger-input text-black"
+              placeholder="Enter your message..."
+              value={msgText}
+              onChange={(e) => setMsgText(e.target.value)}
+            />
+            <button type="submit" className="msger-send-btn">
+              Send
+            </button>
+          </form>
+        </section>
+      )}
     </div>
-  );
-};
+  )
+}
