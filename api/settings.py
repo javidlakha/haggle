3e7ab1b8fe8bb174e3c212084ffbb5a86ada9343
Javid Lakha <javid@javidlakha.com>
2023-10-21 16:37:44 +0100

    get absolute path to .env

--- api/settings.py
+++ api/settings.py
@@ -2,7 +2,10 @@ import pathlib
 from starlette.config import Config
 
 # Configuration from environment variables or '.env' file.
-config = Config(pathlib.Path.cwd() / ".env")
+config = Config(pathlib.Path(__file__).parent.resolve() / ".env")
+
+# GCP API key.
+GCP_API_KEY = config("GCP_API_KEY", default="")
 
 # OpenAI API key.
 OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
