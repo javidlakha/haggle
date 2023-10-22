60af0f0db5c01300dc10febee934cca20c569cc0
Javid Lakha <javid@javidlakha.com>
2023-10-22 12:15:17 +0100

    Parrot easter egg

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,32 @@
-'use client'
-import Image from "next/image";
-import Link from "next/link";
-import { Chat } from "./chat";
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCrow } from "@fortawesome/free-solid-svg-icons"
+import { Chat } from "./chat"
 
 export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between">
       <div className="z-10 w-full items-center inline-block justify-between gap-10 font-mono text-sm lg:flex">
-        <Chat/>
+        <Chat />
         <div className="color-black h-screen w-2/5 bg-gray-500 items-center inline-block p-4 py-16">
           <Image
             priority
             src={`/haggle.png`}
             className="mt-2 ml-2 pb-5"
-            width={270}  
+            width={270}
             height={30}
             alt="Follow us on Twitter"
           />
           Welcome to Haggle where you can practice your skills
+          <div>
+            <Link style={{ position: "fixed", bottom: 0 }} href="parrot">
+              <FontAwesomeIcon icon={faCrow} size="xl" />
+            </Link>
+          </div>
         </div>
       </div>
     </main>
-  );
+  )
 }
